146e6043fddc0068393d2d6d8fc38fa6b88c02b5
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-03 04:45:53 +0600

    api data load in cosole

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('Test app.js');
+const loadAllNews = async () => {
+    const url = 'https://openapi.programming-hero.com/api/news/categories';
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
+
+}
+
+loadAllNews();
