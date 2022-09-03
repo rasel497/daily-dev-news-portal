007cbfdf53100fb39a16c379a6f652a871f892b2
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-03 15:28:33 +0600

    categories list dynamically added nav menu

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
+// alert('HI');
+
 const loadAllNews = async () => {
-    const url = 'https://openapi.programming-hero.com/api/news/categories';
-    const res = await fetch(url);
+    const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
     const data = await res.json();
-    console.log(data.data);
+    dispalyShow(data);
 
 }
 
 loadAllNews();
+
+const dispalyShow = (categories) => {
+    const categoriesName = document.getElementById('categories-name');
+
+    // console.log(categories.data.news_category);
+    const category = categories.data.news_category;
+    console.log(category);
+
+    category.forEach(neswItem => {
+        // console.log(neswItem);
+        const li = document.createElement('li');
+        li.classList.add('nav2');
+        li.innerHTML = `
+        <a href="#">${neswItem.category_name}</a>
+        
+        `;
+        categoriesName.appendChild(li);
+    })
+}
+
+// dispalyShow();
